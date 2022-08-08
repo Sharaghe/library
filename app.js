985d0bb4d3c0ef4dd8eed337f7540795bdba8ef2
Florian Kattner <florian.kattner@gmail.com>
2022-08-08 14:27:53 +0200

    Create input mask mockup & table showing entries

--- app.js
+++ app.js
@@ -8,5 +8,44 @@ function Book(title, author, pages, haveRead){
     }
 }
 
-const hobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, "not read");
-console.log(hobbit.info());
+const table = document.querySelector("table");
+
+let myLibrary = [];
+
+function addBooktoLibrary(book){
+    myLibrary.push(book)
+}
+
+function createNewRow(book){
+    let row = document.createElement("tr");
+
+    let dataTitle = document.createElement("td");
+    dataTitle.innerText = book.title;
+
+    let dataAuthor = document.createElement("td");
+    dataAuthor.innerText = book.author;
+
+    let dataPages = document.createElement("td");
+    dataPages.innerText = book.pages;
+
+    let dataHaveRead = document.createElement("td");
+    dataHaveRead.innerText = book.haveRead;
+
+    row.appendChild(dataTitle);
+    row.appendChild(dataAuthor);
+    row.appendChild(dataPages);
+    row.appendChild(dataHaveRead);
+
+    table.appendChild(row);
+}
+
+function checkForUpdates(){
+    myLibrary.forEach(element => {
+        createNewRow(element);
+    });
+}
+
+const hobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, "no");
+addBooktoLibrary(hobbit);
+
+checkForUpdates();
